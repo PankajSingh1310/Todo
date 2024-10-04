9580241e8f872fb5e5f21524ad7f06b686990dde
PankajSingh1310 <130424788+PankajSingh1310@users.noreply.github.com>
2024-10-04 17:49:14 +0530

    implement note update and delete functionality with complete authentication

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -17,18 +17,44 @@ const userNotes = async (req, res) => {
 const createNote = async (req, res) => {
     const {title, description} = req.body;
 
-    const note = await noteModel.create({
-        title,
-        description
-    })
-    
     const user = req.user;             
     const userExist = await userModel.findOne({email: user.email}).select("-password");
 
+    const note = await noteModel.create({
+        title,
+        description,
+        createdBy: userExist._id
+    })
+    
     userExist.notes.push(note._id);
     await userExist.save();
 
     res.redirect(`/api/user/${userExist._id}/notes`);
 }
 
-module.exports = {userNotes, createNote};
+const deleteNote = async (req, res) => {
+    
+  const deletedNote = await noteModel.findOneAndDelete({_id: req.params.id});
+
+  const user = req.user;             
+  const userExist = await userModel.findOne({email: user.email}).select("-password");
+
+  res.redirect(`/api/user/${userExist._id}/notes`);
+}
+
+const showNoteForUpdate = async (req, res) => {
+  const noteID = req.params.id;
+  const note = await noteModel.findOne({_id: noteID});
+  res.render("update", {note});
+}
+
+const updateNote = async (req, res) => {
+  const {title, description} = req.body;
+  const noteID = req.params.id;
+
+  const deletedNote = await noteModel.findOneAndUpdate({_id: noteID}, {title, description});
+
+  res.redirect(`/api/user/${deletedNote.createdBy}/notes`);
+}
+
+module.exports = {userNotes, createNote, deleteNote, updateNote, showNoteForUpdate};
--- routes/noteRoute.js
+++ routes/noteRoute.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const {createNote} = require("../controllers/noteController");
+const {createNote, deleteNote, updateNote, showNoteForUpdate} = require("../controllers/noteController");
 const authmiddleware = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
 router.post("/create", authmiddleware, createNote);
+router.get("/:id/delete", authmiddleware, deleteNote);
+router.get("/:id/update", authmiddleware, showNoteForUpdate);
+router.post("/:id/update", authmiddleware, updateNote);
 
 module.exports = router;  
